1dbb8e6f1b985fbf0b21fc62481bc102c4d6113e
mafarthi <maxfarthing1@gmail.com>
2024-07-17 18:35:13 -0400

    fixed user login on reload with web token auth

--- client/src/pages/Roots.jsx
+++ client/src/pages/Roots.jsx
@@ -2,18 +2,35 @@ import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import MainNavigation from '../components/MainNavigation'
 import { useAuth } from '../store/AuthContext'
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 export default function RootLayout() {
   const { isAuthenticated, login } = useAuth()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const userId = localStorage.getItem('userId')
-    if(userId) {
-      fetch(`http://localhost:5000/auth/${userId}`)
-      .then(response => response.json())
-      .then(user => {
-        login(user)
-      })
+    const token = localStorage.getItem('token')
+    if(userId && token) {
+      const decodedToken = jwtDecode(token)
+      const currentTime = Date.now() / 1000
+      if(decodedToken.exp < currentTime) {
+        console.log('Token expired')
+        localStorage.clear()
+        navigate('/')
+      } else {
+        fetch(`http://localhost:5000/auth/${userId}`, {
+          headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => response.json())
+        .then(user => {
+          login(user)
+        })
+      }
     }
   }, [isAuthenticated])
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const isauth = require('../middlewares/is-auth')
 
 const authController = require('../controllers/auth')
 
 const router = express.Router()
 
 //  GET*   /auth/:userId
-router.get('/:userId', authController.findUser)
+router.get('/:userId', isauth, authController.findUser)
 
 //  POST*  /auth/signup
 router.post('/signup', authController.signup)
